e05d270ab0f6be023af6d4b5f9d37f17f07f1704
Jannina-RM <99303446+Jannina-RM@users.noreply.github.com>
2024-01-01 16:00:24 +0000

    perfilv1

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Jannina Romero",
+  title: "Industrial Engineer",
+  email: "jannina.romero@unmsm.edu.pe",
+  gitHub: "Jannina-RM",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
