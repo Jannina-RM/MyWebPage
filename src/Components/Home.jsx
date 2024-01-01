26d8b6928d4ad352e395e278ad69fa3f6e049a41
Jannina-RM <99303446+Jannina-RM@users.noreply.github.com>
2024-01-01 16:17:06 +0000

    portada Home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "/workspaces/MyWebPage/src/images/escritoriowomen.jpg";
+import image from "../images/escritoriowomen.jpg";
 
 const imageAltText = "Escritorio de mujer";
 
