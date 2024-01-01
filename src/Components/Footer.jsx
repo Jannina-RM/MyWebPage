dc6701db825c0eb26c3b913d01a207f4090e79ed
Jannina - RM <99303446+Jannina-RM@users.noreply.github.com>
2024-01-01 17:36:48 +0000

    linkedin

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -86,7 +86,7 @@ const Footer = (props) => {
         )}
         {linkedIn && (
           <a
-            href={`https://www.linkedin.com/in/${linkedIn}`}
+            href={`https://www.linkedin.com/in/jannina-romero`}
             target="_blank"
             rel="noopener noreferrer"
           >
