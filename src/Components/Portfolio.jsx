2910b01c9aae14c9f0e785dc4df8423f8e26c2ae
Jannina - RM <99303446+Jannina-RM@users.noreply.github.com>
2024-01-02 04:58:00 +0000

    actualización

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,7 +31,7 @@ const imageAltText = "Woman in desktop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Apply to become in a Microsoft Learning Ambassador in 5 steps! 🎉",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
@@ -46,13 +46,13 @@ const projectList = [
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://github.com/Jannina-RM?tab=repositories",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Introduction to python 🐍 ",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    url: "https://www.python.org/about/gettingstarted/",
   },
 ];
 
